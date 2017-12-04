66f8453fc06ebee6d43f09b2f31a0db72817cec9
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 16:47:12 +0800

    The more you sweat in peace, the less you bleed in war.

--- public/js/frontend_routes.js
+++ public/js/frontend_routes.js
@@ -1,6 +1,11 @@
 function do_routes($routeProvider) {
   console.log('doing FE routes');
   $routeProvider.when(
+    '/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'login_controller'
+    }
+  ).when(
     '/', {
       templateUrl: 'partials/home.html',
       controller: 'home_controller'
