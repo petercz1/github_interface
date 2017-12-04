31d97075f6120b3f5e8d1ce9c869325686187852
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 09:04:36 +0800

    first commit for github_interface

---
+++ mongodb/mongoose_connection.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var url = 'mongodb://localhost:27500/github';
+
+mongoose.connect(url, {
+  useMongoClient: true
+});
+
+var git_doc = {
+  user: String,
+
+}
+
+var git_schema = new mongoose.Schema(git_doc);
+var Gituser = mongoose.model('git_user', git_schema);
+
+module.exports =
