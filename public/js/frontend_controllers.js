f21e120b00850df8ec8b62e0b6d021c3683e7359
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 15:51:28 +0800

    "You can't teach seven foot." -- Frank Layton, Utah Jazz basketball coach, when asked why he had recruited    a seven-foot tall auto mechanic

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -2,7 +2,7 @@ function do_home_controller($scope, $http) {
     console.log('doing home controller');
 }
 
-function do_login_controller($scope, $http) {
+function do_login_controller($scope, $http, $) {
     console.log('doing login controller');
     $http.get('/api/v1/auth').then(
         function (result) {
