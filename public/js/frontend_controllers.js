1c46acc784e6ab347ed3ea6ccbeed996105600b6
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 15:39:44 +0800

    Hummingbirds never remember the words to songs.

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -4,5 +4,10 @@ function do_home_controller($scope, $http) {
 
 function do_login_controller($scope, $http) {
     console.log('doing login controller');
-    $http.get('/api/v1/auth', )
+    $http.get('/api/v1/auth').then(
+        function (result) {
+            console.log(result);
+            
+        }
+    )
 }
