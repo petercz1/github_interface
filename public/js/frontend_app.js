1525c4235fd195ce6245d1471263e31cdb4cb07e
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 11:21:45 +0800

    Q:	How many Marxists does it take to screw in a light bulb? A:	None:  The light bulb contains the seeds of its own revolution.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,4 +1,9 @@
 console.log('loaded frontend app');
 
 var frontend_app = angular.module('github_app', ['ngRoute']);
-frontend_app.config()
+frontend_app.config(do_routes);
+frontend_app.controller('login', do_login);
+
+function do_login($scope, $http) {
+    
+}
