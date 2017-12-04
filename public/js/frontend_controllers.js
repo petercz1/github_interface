858078a6190a06f55903f6bbf02a9d94c8600197
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 15:52:34 +0800

    :-) your own self. 		-- Larry Wall in <199709261754.KAA23761@wall.org>

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -2,9 +2,10 @@ function do_home_controller($scope, $http) {
     console.log('doing home controller');
 }
 
-function do_login_controller($scope, $http, $) {
+function do_login_controller($scope, $http, $routeParams) {
     console.log('doing login controller');
-    $http.get('/api/v1/auth').then(
+    console.log($routeParams);
+    $http.get('/api/v1/auth/' + routeParams.input_user).then(
         function (result) {
             console.log(result);
         }
