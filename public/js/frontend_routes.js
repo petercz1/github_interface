c362028bff5bf7dd408ec2f2ec96657d4960d680
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 16:23:37 +0800

    Ocean:  A body of water occupying about two-thirds of a world made for man -- who has no gills. 		-- Ambrose Bierce

--- public/js/frontend_routes.js
+++ public/js/frontend_routes.js
@@ -1,11 +1,6 @@
 function do_routes($routeProvider) {
   console.log('doing FE routes');
   $routeProvider.when(
-    '/login', {
-      templateUrl: 'partials/login.html',
-      controller: 'login_controller'
-    }
-  ).when(
     '/', {
       templateUrl: 'partials/home.html',
       controller: 'home_controller'
