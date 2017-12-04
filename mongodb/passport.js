5f021aa9eb7ecbe575ab49f59e04996d556b09e1
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 10:08:43 +0800

    I'd love to kiss you, but I just washed my hair. 		-- Bette Davis, "Cabin in the Cotton"

--- mongodb/passport.js
+++ mongodb/passport.js
@@ -7,16 +7,19 @@ var git_creds = {
   clientSecret: creds.client_secret,
   callbackURL: creds.callback_url
 };
-var git_strategy = new GitHubStrategy();
+var git_strategy = new GitHubStrategy(git_creds, get_access);
 
+passport.use(git_strategy);
+
+function(accessToken, refreshToken, profile, done) {
+  User.findOrCreate({ githubId: profile.id }, function (err, user) {
+    return done(err, user);
+  });
+}
 passport.use(new GitHubStrategy({
     clientID: creds.client_id,
     clientSecret: creds.client_secret,
     callbackURL: creds.callback_url
   },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
+  
 ));
