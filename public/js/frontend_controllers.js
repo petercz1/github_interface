17f5d5ccba488e8a14cc149424860e255b564559
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 16:46:07 +0800

    "If Jesus came back today, and saw what was going on in his name, he'd never stop throwing up." 		-- Max Von Sydow's character in "Hannah and Her Sisters"

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -1,3 +1,13 @@
 function do_home_controller($scope, $http) {
     console.log('doing home controller');
 }
+
+function do_login_controller($scope, $http, $routeParams) {
+    console.log('doing login controller');
+    console.log($routeParams);
+    $http.get('/api/v1/auth').then(
+        function (result) {
+            console.log(result);
+        }
+    )
+}
