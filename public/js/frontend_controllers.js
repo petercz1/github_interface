fcc356b17c6a554f72683055ab78c8035beee70a
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 18:24:03 +0800

    What the large print giveth, the small print taketh away.

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -2,12 +2,13 @@ function do_home_controller($scope, $http) {
     console.log('doing home controller');
 }
 
-function do_login_controller($scope, $http, $routeParams) {
+function do_login_controller($scope, $http, $routeParams, $location) {
     console.log('doing login controller');
     console.log($routeParams);
     $http.get('/api/v1/auth').then(
         function (result) {
             console.log(result);
+            $location.path()
         }
     )
 }
