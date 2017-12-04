6981dcf6b084986985f346ccbeb71a4a99c5fcad
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 11:25:01 +0800

    "The Schizophrenic: An Unauthorized Autobiography"

--- public/js/frontend_routes.js
+++ public/js/frontend_routes.js
@@ -1,6 +1,11 @@
 function do_routes($routeProvider) {
     console.log('doing FE routes');
     $routeProvider.when(
+        '/login',{
+            templateUrl: 'partials/login.html',
+            controller: 'login_controller'
+        }
+    ).when(
         
-    ).when();
+    );
 }
