fbc83183cdca860dc23cfa26ea423954ed303aa8
petercz1 <email.peter.carroll@gmail.com>
2017-12-04 10:07:38 +0800

    Visit beautiful Wisconsin Dells.

--- mongodb/passport.js
+++ mongodb/passport.js
@@ -2,8 +2,13 @@ var passport = require('passport');
 var github_strategy = require('passport-github2');
 var creds = require('./creds');
 
+var git_creds = {
+  clientID: creds.client_id,
+  clientSecret: creds.client_secret,
+  callbackURL: creds.callback_url
+};
+var git_strategy = new GitHubStrategy();
 
-var git_strategy = new GitHubStrategy()
 passport.use(new GitHubStrategy({
     clientID: creds.client_id,
     clientSecret: creds.client_secret,
