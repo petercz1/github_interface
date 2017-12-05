bfe411748b1de77459c716120de012bddacc8d84
petercz1 <email.peter.carroll@gmail.com>
2017-12-05 14:24:57 +0800

    Is your job running?  You'd better go catch it!

--- mongodb/passport.js
+++ mongodb/passport.js
@@ -1,32 +1,34 @@
-function name(params) {
-  
+function build_passport(passport) {
+
+
+
+  var GitHubStrategy = require('passport-github2').Strategy;
+  var creds = require('./creds.js');
+
+  var git_creds = {
+    clientID: creds.client_id,
+    clientSecret: creds.client_secret,
+    callbackURL: creds.callback_url
+  };
+
+  function get_access(accessToken, refreshToken, profile, done) {
+    console.log('getting access');
+    console.log(profile);
+    return done(null, profile);
+  }
+
+  var git_strategy = new GitHubStrategy(git_creds, get_access);
+
+  passport.use(git_strategy);
+
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+  });
+  return passport;
 }
 
-var GitHubStrategy = require('passport-github2').Strategy;
-var creds = require('./creds.js');
-
-var git_creds = {
-  clientID: creds.client_id,
-  clientSecret: creds.client_secret,
-  callbackURL: creds.callback_url
-};
-
-function get_access(accessToken, refreshToken, profile, done) {
-  console.log('getting access');
-  console.log(profile);
-  return done(null, profile);
-}
-
-var git_strategy = new GitHubStrategy(git_creds, get_access);
-
-passport.use(git_strategy);
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-module.exports = passport;
+module.exports = build_passport;
