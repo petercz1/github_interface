9b10a9cd8ed9d33e8a7131eb94cab4991ebe9a7c
petercz1 <email.peter.carroll@gmail.com>
2017-12-05 14:28:14 +0800

    Roses are red; 	Violets are blue. I'm schizophrenic, 	And so am I.

--- routes/backend_routes.js
+++ routes/backend_routes.js
@@ -1,43 +1,43 @@
-function build_router(passport){
+function build_router(passport) {
 
-var router = require('express').Router();
-var passport = require('../mongodb/passport')(passport);
+  var router = require('express').Router();
+  var passport = require('../mongodb/passport')(passport);
 
-router.get('/', do_homepage);
+  router.get('/', do_homepage);
 
-function do_homepage(req, res) {
-  console.log('doing homepage');
-  res.sendfile('index.html');
-}
+  function do_homepage(req, res) {
+    console.log('doing homepage');
+    res.sendfile('index.html');
+  }
 
-// auth
-router.get('/api/v1/auth', passport.authenticate('github', {
-  scope: ['user:email']
-}));
+  // auth
+  router.get('/api/v1/auth', passport.authenticate('github', {
+    scope: ['user:email']
+  }));
 
-router.get('/api/v1/git_callback',
-  passport.authenticate('github', {
-    failureRedirect: '/failed'
-  }), do_authenticated);
+  router.get('/api/v1/git_callback',
+    passport.authenticate('github', {
+      failureRedirect: '/failed'
+    }), do_authenticated);
 
-router.get('/failed', do_failed);
+  router.get('/failed', do_failed);
 
-function do_failed(req, res) {
-  console.log('doing failed');
-  console.log(req.body);
-  res.json({
-    message: 'failed login'
-  });
-}
+  function do_failed(req, res) {
+    console.log('doing failed');
+    console.log(req.body);
+    res.json({
+      message: 'failed login'
+    });
+  }
 
-function do_authenticated(req, res) {
-  console.log('authenticated!');
-  // Successful authentication, redirect.
-  res.json({
-    message: 'authenticated - proceed!'
-  });
-}
-return router;
+  function do_authenticated(req, res) {
+    console.log('authenticated!');
+    // Successful authentication, redirect.
+    res.json({
+      message: 'authenticated - proceed!'
+    });
+  }
+  return router;
 }
 
 module.exports = build_router;
