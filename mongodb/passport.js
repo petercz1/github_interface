b1712ad99979e2ecf761ce8d19d974024c037bdc
petercz1 <email.peter.carroll@gmail.com>
2017-12-05 20:35:46 +0800

    Obviously I was either onto something, or on something. 		-- Larry Wall on the creation of Perl

--- mongodb/passport.js
+++ mongodb/passport.js
@@ -8,7 +8,6 @@ function build_passport(passport) {
     clientSecret: creds.client_secret,
     callbackURL: creds.callback_url
   };
-  console.log(git_creds);
 
   function get_access(accessToken, refreshToken, profile, done) {
     console.log('getting access');
