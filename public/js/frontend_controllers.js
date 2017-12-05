55e8db6f0248e7ab59faa1d6e20562f983d730e3
petercz1 <email.peter.carroll@gmail.com>
2017-12-05 10:09:29 +0800

    "Would I turn on the gas if my pal Mugsy were in there?"    "You might, rabbit, you might!" 		-- Looney Tunes, Bugs and Thugs (1954, Friz Freleng)

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -5,7 +5,7 @@ function do_home_controller($scope, $http) {
 function do_login_controller($scope, $http, $routeParams, $location) {
     console.log('pre login');
     console.log($routeParams);
-    $http.get('/api/v1/auth').then(
+    $http.jsonp('/api/v1/auth').then(
         function (result) {
             console.log('post login');
             console.log(result);
