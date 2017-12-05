d2c4cc25ff7e659d62c1f6bb1e11308c9b30249c
petercz1 <email.peter.carroll@gmail.com>
2017-12-06 06:51:34 +0800

    The best laid plans of mice and men are usually about equal. 		-- Blair

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -1,15 +1,5 @@
 function do_home_controller($scope, $http) {
   console.log('doing home controller');
-//   $scope.login = function () {
-//     console.log('login clicked');
-//     $http.get('/api/v1/auth').then(
-//       function (result) {
-//         console.log('post login');
-//         console.log(result);
-//         $location.path('logged_in'); // up to here!
-//       }
-//     )
-//   }
 }
 
 function do_login_controller($scope, $http, $routeParams, $location) {
