09d8c9cbc45f97a41b6c8ca097bcbfd873fd700a
petercz1 <email.peter.carroll@gmail.com>
2017-12-05 14:42:29 +0800

    Year, n.: 	A period of three hundred and sixty-five disappointments. 		-- Ambrose Bierce, "The Devil's Dictionary"

--- routes/backend_routes.js
+++ routes/backend_routes.js
@@ -13,14 +13,11 @@ function build_router(passport) {
   // auth
   router.get('/api/v1/auth', passport.authenticate('github', {
     scope: ['user:email']
-  }), function (req, res, next) {
-    req.session.returnTo = '/#' + req.query.returnTo; 
-    next();
-  });
+  }));
 
   router.get('/api/v1/git_callback', function (req, res, next) {
     passport.authenticate('github', {
-      successRedirect: req.session.returnTo,
+      successRedirect: '/logged_in',
       failureRedirect: '/failed'
     })
   }, do_authenticated);
