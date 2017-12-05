4a8a019998bfff1e44dc294956e849cb9add7d9a
petercz1 <email.peter.carroll@gmail.com>
2017-12-05 14:19:28 +0800

    Did you ever walk into a room and forget why you walked in?  I think that's how dogs spend their lives. 		-- Sue Murphy

--- backend_app.js
+++ backend_app.js
@@ -2,14 +2,12 @@ var express = require('express');
 var backend_app = express();
 var bodyParser = require('body-parser');
 var routes = require('./routes/backend_routes');
-var cors = require('cors');
 var port = 4000;
 
 //backend_app.set('view engine', 'ejs');
 backend_app.use(express.static('public'));
 backend_app.use(bodyParser.urlencoded({extended: true}));
 backend_app.use(bodyParser.json());
-backend_app.use(cors());
 
 backend_app.use(routes);
 
