f9175eb7777bca24216f325a5a9393233373f27a
petercz1 <email.peter.carroll@gmail.com>
2017-12-06 06:52:40 +0800

    < asuffield> a workstation is anything you can stick on somebodies desk              and con them into using 		-- in #debian-devel

--- public/js/frontend_controllers.js
+++ public/js/frontend_controllers.js
@@ -2,8 +2,6 @@ function do_home_controller($scope, $http) {
   console.log('doing home controller');
 }
 
-function do_login_controller($scope, $http, $routeParams, $location) {
-  console.log('pre login');
-  console.log($routeParams);
-
+function do_login_controller($scope, $http) {
+  console.log('doing login controller');
 }
