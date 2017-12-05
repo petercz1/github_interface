d482c44872685ad4fb21bbc0287942ea040962b5
petercz1 <email.peter.carroll@gmail.com>
2017-12-05 14:20:34 +0800

    Authors are easy to get on with -- if you're fond of children. 		-- Michael Joseph, "Observer"

--- backend_app.js
+++ backend_app.js
@@ -2,13 +2,15 @@ var express = require('express');
 var backend_app = express();
 var bodyParser = require('body-parser');
 var routes = require('./routes/backend_routes');
+var passport = require('passport');
 var port = 4000;
 
 //backend_app.set('view engine', 'ejs');
 backend_app.use(express.static('public'));
 backend_app.use(bodyParser.urlencoded({extended: true}));
 backend_app.use(bodyParser.json());
-
+backend_app.use(passport.initialize());
+backend_app.use(passport.session());
 backend_app.use(routes);
 
 backend_app.listen(port, listening);
--- mongodb/passport.js
+++ mongodb/passport.js
@@ -1,4 +1,3 @@
-var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
 var creds = require('./creds.js');
 
