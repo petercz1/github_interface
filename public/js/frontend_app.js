c4fe30317bc8298f5a2aefac599687467434a2e3
petercz1 <email.peter.carroll@gmail.com>
2017-12-06 15:51:40 +0800

    When you are in it up to your ears, keep your mouth shut.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,6 +1,6 @@
 console.log('loaded frontend app');
 
-var frontend_app = angular.module('github_app', ['ngRoute']);
+var frontend_app = angular.module('github_app', ['ngRoute', 'gm.dragDrop']);
 frontend_app.config(do_routes);
 
 frontend_app.controller('home_controller', do_home_controller);
