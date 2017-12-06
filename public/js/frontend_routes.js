6b2f35700cd53dd6faa26f122110baedba0bef91
petercz1 <email.peter.carroll@gmail.com>
2017-12-06 09:17:59 +0800

    Is a tattoo real, like a curb or a battleship?  Or are we suffering in Safeway?

--- public/js/frontend_routes.js
+++ public/js/frontend_routes.js
@@ -1,18 +1,23 @@
 function do_routes($routeProvider) {
   console.log('doing FE routes');
-  $routeProvider.when(
-    '/login', {
-      templateUrl: 'partials/login.html',
-      controller: 'login_controller'
-    }
-  ).when(
-    '/', {
-      templateUrl: 'partials/home.html',
-      controller: 'home_controller'
-    }
-  ).when('/logged_in', {
-    templateUrl: 'partials/logged_in.html'
-  }).when('/failed', {
-    templateUrl: 'partials/failed.html'
-  });
+  $routeProvider
+    .when(
+      '/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'login_controller'
+      }
+    )
+    .when(
+      '/', {
+        templateUrl: 'partials/home.html',
+        controller: 'home_controller'
+      }
+    )
+    .when('/logged_in', {
+      templateUrl: 'partials/logged_in.html'
+    })
+    .when('/failed', {
+      templateUrl: 'partials/failed.html'
+    })
+    .when('/authToken');
 }
