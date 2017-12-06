16000b237392867ee339415e59e5bb47015cc37b
petercz1 <email.peter.carroll@gmail.com>
2017-12-06 09:55:39 +0800

    I finally went to the eye doctor.  I got contacts.  I only need them to read, so I got flip-ups. 		-- Steven Wright

--- backend_app.js
+++ backend_app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var backend_app = express();
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var routes = require('./routes/backend_routes')(passport);
 var port = 4000;
