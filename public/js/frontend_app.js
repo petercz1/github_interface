8e3eff68cae3bb00825eecbd141a956956a772c2
petercz1 <email.peter.carroll@gmail.com>
2017-12-06 15:33:11 +0800

    Why is it called a funny bone when it hurts so much?

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,6 +1,6 @@
 console.log('loaded frontend app');
 
-var frontend_app = angular.module('github_app', ['ngRoute', require('angular-drag-drop')]);
+var frontend_app = angular.module('github_app', ['ngRoute', 'angular-drag-drop']);
 frontend_app.config(do_routes);
 
 frontend_app.controller('home_controller', do_home_controller);
