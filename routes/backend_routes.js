1d11bb016a9fec45aab5e45cd6fac8a97266741e
petercz1 <email.peter.carroll@gmail.com>
2017-12-08 09:06:24 +0800

    Just about every computer on the market today runs Unix, except the Mac (and nobody cares about it). 		-- Bill Joy 6/21/85

--- routes/backend_routes.js
+++ routes/backend_routes.js
@@ -14,7 +14,12 @@ function build_router(passport) {
   // data---------------------------------------
   router.get('/api/v1/github_data', do_github_data);
   router.get('/api/v1/repositories', do_repositories);
-  router.delete('/api/v4/delete/:id')
+  router.delete('/api/v4/delete/:id', do_delete);
+
+  function do_delete(req, res) {
+    console.log(req.params);
+    // TODO - delete rep from github!
+  }
 
   function do_github_data(req, res) {
     console.log('doing BE github data');
